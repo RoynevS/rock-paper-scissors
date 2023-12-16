fe5eb541253ec7bd557afee35bde75bbbf46f6ab
RoynevS <109765130+RoynevS@users.noreply.github.com>
2023-12-16 11:49:25 +0100

    Add function to play a best of five
    add function that plays 5 rounds and displays winner after every round

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-// create function getComputerChoice
-// return random selection out of rock, paper, scissor
-
 const choices = ["Rock", "Paper", "Scissors"];
 
 const getRandomNumber = arr => {
   return Math.floor(Math.random() * arr.length)
 }
 
+// TODO: put choices in this function to not have it global
 const getComputerChoice = () =>  choices[getRandomNumber(choices)];
 
 const playRound = (playerSelection, computerSelection) => {
-  playerSelection = playerSelection.toLowerCase()
-  computerSelection = computerSelection.toLowerCase()
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
     return 0
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats Rock"
+    return "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats Scissors"
+    return "You win! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats Rock"
+    return "You win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beat Paper"
+    return "You lose! Scissors beat Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock beats Scissors"
+    return "You lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats Paper"
+    return "You win! Scissors beats Paper";
   }
 }
+
+// play best of five
+// keeps score
+// reports winner after every round
+// reports winner at the end
+// takes user input
+
+const game = () => {
+  for (let i = 1; i <= 5; i++) {
+    playerSelection = prompt('Choose between "Rock", "Paper" and "Scissors":');
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection)) 
+  }
+}
+
+game()
