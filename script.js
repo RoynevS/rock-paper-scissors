5cb2feb739da528c094412642376adccde0348bd
RoynevS <109765130+RoynevS@users.noreply.github.com>
2023-12-16 11:38:10 +0100

    Add function to play a round
    takes player selection and computer selection and compares the two to
    decide a winner. returns a string declaring the winner.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // create function getComputerChoice
 // return random selection out of rock, paper, scissor
 
-const choices = ["Rock", "Paper", "Scissors"]
+const choices = ["Rock", "Paper", "Scissors"];
 
 const getRandomNumber = arr => {
   return Math.floor(Math.random() * arr.length)
@@ -9,4 +9,23 @@ const getRandomNumber = arr => {
 
 const getComputerChoice = () =>  choices[getRandomNumber(choices)];
 
-console.log(getComputerChoice())
+const playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase()
+  computerSelection = computerSelection.toLowerCase()
+
+  if (playerSelection === computerSelection) {
+    return 0
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock"
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors"
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock"
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beat Paper"
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors"
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper"
+  }
+}
